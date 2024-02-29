4cd7de7303e17e12fb9a287bd018586320d8d1b5
noprobelm <noprobelm@protonmail.com>
2024-02-29 17:08:17 -0500

    Added __main__ module

---
+++ cgol/__main__.py
@@ -0,0 +1,33 @@
+"""Main entrypoint for running a simulation from the command line"""
+
+from .args import parse_args
+from .renderer import Renderer
+
+from glipy.cell import MooreCell
+from glipy.state import ConwayState
+
+from . import console
+
+
+def main() -> None:
+    """Main entrypoint for running an automaton from the command line"""
+    result = parse_args()
+
+    renderer = Renderer(
+        cell_type=MooreCell,
+        initial_state=ConwayState,
+        xmax=console.width,
+        ymax=console.height * 2,
+    )
+    try:
+        renderer.spawn(renderer.midpoint - result.automaton.midpoint, result.automaton)
+        renderer.run(**result.start_kwargs)
+    except Exception as e:
+        print(
+            f"{renderer.xmax}, {renderer.ymax}, {result.automaton.xmax}, {result.automaton.ymax}"
+        )
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
